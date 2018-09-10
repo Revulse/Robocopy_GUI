9a3b03431ca0dfd5d42734888f94af60fdd9f116
Justin DeGolyer <31967031+Revulse@users.noreply.github.com>
2018-09-10 16:44:04 -0400

    Working on If Else Statement

--- Robocopy GUI/Form1.cs
+++ Robocopy GUI/Form1.cs
@@ -19,8 +19,31 @@ namespace Robocopy_GUI
         {
             string sourceDir = textBox1.Text;
             string destinationDir = textBox2.Text;
-            string command = "dir";
-            System.Diagnostics.Process.Start("cmd.exe", command);
+            System.Diagnostics.Process.Start("cmd.exe", Switches);
+        }
+
+        private string Switches()
+        {
+            if (radio_button_Mirror.Checked == true
+                && checkBox1.Checked == false)
+            {
+                return "robocopy ";
+            }
+            else if (radio_button_CopyOverNewFiles.Checked == true
+                && checkBox1.Checked == false)
+            {
+
+            }
+            else if (radio_button_CopyOverNewFiles.Checked == true
+                && checkBox1.Checked == true)
+            {
+
+            }
+            else
+            {
+                return "Invalid Selection";
+            }
+
         }
     }
 }
